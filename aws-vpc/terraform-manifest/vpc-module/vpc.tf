6753fa7a9d4251e241c7b5ff5fa540129b6cd1e1
Leong <hui.yee.leong@accenture.com>
2022-07-19 09:44:39 +1000

    Updated AWS VPC

--- aws-vpc/terraform-manifest/vpc-module/vpc.tf
+++ aws-vpc/terraform-manifest/vpc-module/vpc.tf
@@ -5,8 +5,8 @@ module "vpc" {
   #VPC Basic Details
   name = "vpc-dev"
   cidr = "10.0.0.0/16" # 10.0.0.0/8 is reserved for EC2-Classic
-  azs                 = ["us-east-1a", "us-east-1a"]
-  private_subnets     = ["10.0.1.0/16", "10.0.2.0/16"]
+  azs                 = ["us-east-1a", "us-east-1b"]
+  private_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets      = ["10.0.101.0/24", "10.0.102.0/24"]
 
   #Database Subnets
