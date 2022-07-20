a140b7761e426d413d480900b34b74cf83262c94
Leong <hui.yee.leong@accenture.com>
2022-07-20 15:20:30 +1000

    Added new groovy

---
+++ aws-ec2/jenkins.groovy
@@ -0,0 +1,28 @@
+#!/usr/bin/env groovy
+
+pipeline {
+
+    agent { label 'ecs' }
+
+    parameters {
+        string(name: 'account', description: 'Deploy into AWS Account', defaultValue: "aws-account" )
+        string(name: 'ProjectName', description: 'Provide the unique stack name. MANDATORY field.', defaultValue: 'aws-terraform-project')
+       
+       
+    }
+
+    environment {
+        AWS_CREDENTIALS = "${params.account}"
+    }
+
+    stages {
+
+        stage('initialise-aws-terraform-project') {
+            steps {
+                script {
+                    currentBuild.displayName = "#${env.BUILD_NUMBER} - ${params.action}"
+                    currentBuild.description = "Build #${env.BUILD_NUMBER} - Deploying to ${params.account} - ProjectName ${params.ProjectName} "
+                    sh 'printenv'
+                }
+            }
+        }
