b5003fb67b13eb63dc056caffbcb89527c40da45
Leong <hui.yee.leong@accenture.com>
2022-07-26 12:06:03 +1000

    Updated the jenkins groovy script

--- aws-ec2/jenkins.groovy
+++ aws-ec2/jenkins.groovy
@@ -24,7 +24,7 @@ pipeline {
                     currentBuild.description = "Build #${env.BUILD_NUMBER} - Deploying to ${params.account} - ProjectName ${params.ProjectName} "
                     sh 'printenv'
                     sh 'aws s3 ls'
-                    sh 'cd /aws-ec2/terraform-manifest'
+                    sh 'cd aws-ec2/terraform-manifest'
                 }
             }
         }
