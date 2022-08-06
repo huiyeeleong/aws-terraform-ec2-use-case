cc7545cdcdf9f6d3a9d9a8495997ff71369a2ffc
Leong <hui.yee.leong@accenture.com>
2022-08-06 23:13:55 +1000

    Updated New Stack

---
+++ aws-alb/terraform-manifest/c10-02-ALB-application-loadbalancer.tf
@@ -0,0 +1,55 @@
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name = "my-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.vpc.vpc_id
+  subnets            = [module.vpc.public_subnets[0], module.vpc.public_subnets[1]]
+  security_groups    = [module.loadbalancer_sg.this_security_group_id]
+
+  access_logs = {
+    bucket = "my-alb-logs"
+  }
+
+  target_groups = [
+    {
+      name_prefix      = "pref-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = "i-0123456789abcdefg"
+          port = 80
+        }
+        my_other_target = {
+          target_id = "i-a1b2c3d4e5f6g7h8i"
+          port = 8080
+        }
+      }
+    }
+  ]
+
+  https_listeners = [
+    {
+      port               = 443
+      protocol           = "HTTPS"
+      target_group_index = 0
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "Test"
+  }
+}
