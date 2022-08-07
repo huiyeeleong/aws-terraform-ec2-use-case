2f7ae330a34e7547ab251af275625ab040b256d9
Leong <hui.yee.leong@accenture.com>
2022-08-07 21:27:05 +1000

    Updated ALB Stack

--- aws-alb/terraform-manifest/c10-02-ALB-application-loadbalancer.tf
+++ aws-alb/terraform-manifest/c10-02-ALB-application-loadbalancer.tf
@@ -1,55 +1,58 @@
+# Terraform AWS Application Load Balancer (ALB)
 module "alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
+  version = "5.16.0"
 
-  name = "my-alb"
-
+  name = "${local.name}-alb"
   load_balancer_type = "application"
-
-  vpc_id             = module.vpc.vpc_id
-  subnets            = [module.vpc.public_subnets[0], module.vpc.public_subnets[1]]
-  security_groups    = [module.loadbalancer_sg.this_security_group_id]
-
-  access_logs = {
-    bucket = "my-alb-logs"
-  }
-
-  target_groups = [
-    {
-      name_prefix      = "pref-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = "i-0123456789abcdefg"
-          port = 80
-        }
-        my_other_target = {
-          target_id = "i-a1b2c3d4e5f6g7h8i"
-          port = 8080
-        }
-      }
-    }
+  vpc_id = module.vpc.vpc_id
+  subnets = [
+    module.vpc.public_subnets[0],
+    module.vpc.public_subnets[1]
   ]
-
-  https_listeners = [
-    {
-      port               = 443
-      protocol           = "HTTPS"
-      target_group_index = 0
-    }
-  ]
-
+  security_groups = [module.loadbalancer_sg.this_security_group_id]
+  # Listeners
   http_tcp_listeners = [
     {
       port               = 80
       protocol           = "HTTP"
-      target_group_index = 0
+      target_group_index = 0 # App1 TG associated to this listener
     }
+  ]  
+  # Target Groups
+  target_groups = [
+    # App1 Target Group - TG Index = 0
+    {
+      name_prefix          = "app1-"
+      backend_protocol     = "HTTP"
+      backend_port         = 80
+      target_type          = "instance"
+      deregistration_delay = 10
+      health_check = {
+        enabled             = true
+        interval            = 30
+        path                = "/app1/index.html"
+        port                = "traffic-port"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 6
+        protocol            = "HTTP"
+        matcher             = "200-399"
+      }
+      protocol_version = "HTTP1"
+      # App1 Target Group - Targets
+      targets = {
+        my_app1_vm1 = {
+          target_id = module.ec2_private.id[0]
+          port      = 80
+        },
+        my_app1_vm2 = {
+          target_id = module.ec2_private.id[1]
+          port      = 80
+        }
+      }
+      tags =local.common_tags # Target Group Tags
+    }  
   ]
-
-  tags = {
-    Environment = "Test"
-  }
+  tags = local.common_tags # ALB Tags
 }
