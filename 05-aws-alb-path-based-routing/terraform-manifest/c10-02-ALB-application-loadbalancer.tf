3d70be3d89d346febc773c0c3c75d59aa97657e3
Leong <hui.yee.leong@accenture.com>
2022-08-08 14:03:24 +1000

    Updated Stacl

--- 05-aws-alb-path-based-routing/terraform-manifest/c10-02-ALB-application-loadbalancer.tf
+++ 05-aws-alb-path-based-routing/terraform-manifest/c10-02-ALB-application-loadbalancer.tf
@@ -48,16 +48,91 @@ module "alb" {
       # App1 Target Group - Targets
       targets = {
         my_app1_vm1 = {
-          target_id = module.ec2_private.id[0]
+          target_id = module.ec2_private_app1.id[0]
           port      = 80
         },
         my_app1_vm2 = {
-          target_id = module.ec2_private.id[1]
+          target_id = module.ec2_private_app1.id[1]
+          port      = 80
+        }
+      }
+      tags =local.common_tags # Target Group Tags
+    },
+    {
+      name_prefix          = "app2-"
+      backend_protocol     = "HTTP"
+      backend_port         = 80
+      target_type          = "instance"
+      deregistration_delay = 10
+      health_check = {
+        enabled             = true
+        interval            = 30
+        path                = "/app2/index.html"
+        port                = "traffic-port"
+        healthy_threshold   = 3
+        unhealthy_threshold = 3
+        timeout             = 6
+        protocol            = "HTTP"
+        matcher             = "200-399"
+      }
+      protocol_version = "HTTP1"
+      # App1 Target Group - Targets
+      targets = {
+        my_app2_vm1 = {
+          target_id = module.ec2_private_app2.id[0]
+          port      = 80
+        },
+        my_app2_vm2 = {
+          target_id = module.ec2_private_app2.id[1]
           port      = 80
         }
       }
       tags =local.common_tags # Target Group Tags
     }  
   ]
+  https_listeners = [
+    # HTTPS Listener Index = 0 for HTTPS 443
+    {
+      port               = 443
+      protocol           = "HTTPS"
+      certificate_arn    = module.acm.this_acm_certificate_arn
+      action_type = "fixed-response"
+      fixed_response = {
+        content_type = "text/plain"
+        message_body = "Fixed Static message - for Root Context"
+        status_code  = "200"
+      }
+    }, 
+  ]
+
+  https_listener_rules = [
+    # Rule-1: /app1* should go to App1 EC2 Instances
+    { 
+      https_listener_index = 0
+      actions = [
+        {
+          type               = "forward"
+          target_group_index = 0
+        }
+      ]
+      conditions = [{
+        path_patterns = ["/app1*"]
+      }]
+    },
+    # Rule-2: /app2* should go to App2 EC2 Instances    
+    {
+      https_listener_index = 0
+      actions = [
+        {
+          type               = "forward"
+          target_group_index = 1
+        }
+      ]
+      conditions = [{
+        path_patterns = ["/app2*"]
+      }]
+    },    
+  ]
+
   tags = local.common_tags # ALB Tags
 }
